cf647dceb9896f35e1b5856a0d3acd0e91063e70
Rustem Mubarakshin <mubarakshin1996@gmail.com>
2023-09-13 20:51:09 +0300

    use-effect-with-async-function

--- src/PersonScore.tsx
+++ src/PersonScore.tsx
@@ -4,7 +4,11 @@ import { getPerson } from "./getPerson";
 
 export function PersonScore() {
   useEffect(() => {
-    getPerson().then((person) => console.log(person));
+    async function getThePerson() {
+      const person = await getPerson();
+      console.log(person);
+    }
+    getThePerson();
   }, []);
   return null;
 }
