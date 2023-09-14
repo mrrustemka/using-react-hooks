9c5e2feccbf8da78ee6078e749a4bdf78414f99d
Rustem Mubarakshin <mubarakshin1996@gmail.com>
2023-09-14 07:58:52 +0300

    create-useState

--- src/PersonScore.tsx
+++ src/PersonScore.tsx
@@ -1,14 +1,29 @@
 /* eslint-disable prettier/prettier */
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { getPerson } from "./getPerson";
 
 export function PersonScore() {
+  const [name, setName] = useState<string | undefined>();
+  const [score, setScore] = useState(0);
+  const [loading, setLoading] = useState(true);
+
   useEffect(() => {
-    async function getThePerson() {
-      const person = await getPerson();
-      console.log(person);
-    }
-    getThePerson();
+    getPerson().then((person) => {
+      setLoading(false);
+      setName(person.name);
+    });
   }, []);
-  return null;
+  if (loading) {
+    return <div>Loading ...</div>;
+  }
+  return (
+    <div>
+      <h3>
+        {name}, {score}
+      </h3>
+      <button>Add</button>
+      <button>Subtract</button>
+      <button>Reset</button>
+    </div>
+  );
 }
